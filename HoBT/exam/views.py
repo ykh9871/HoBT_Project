06dc2520873fe53053b1b501393149b99ef6de8d
Atopiano <121912367+Atopiano@users.noreply.github.com>
2023-04-26 09:47:02 +0900

    ãindex, exam add

--- HoBT/exam/views.py
+++ HoBT/exam/views.py
@@ -2,12 +2,13 @@ from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from .models import Problem
-from django.shortcuts import redirect
+from .models import Problem, Hobt1
 
 
-@staff_member_required
-@user_passes_test(lambda u: u.is_superuser)
+def exam(request):
+    hobt_1_list = Hobt1.objects.all()
+    return render(request, 'exam/exam.html', {'hobt_1': hobt_1_list})
+
 def add_problem(request):
     if request.method == 'POST':
         qid = request.POST.get('qid')
@@ -27,3 +28,31 @@ def add_problem(request):
 def show_problems(request):
     problems = Problem.objects.all()
     return render(request, 'exam/problem_list.html', {'problems': problems})
+
+@staff_member_required
+@user_passes_test(lambda u: u.is_superuser)
+def add_selected_problems(request):
+    if request.method == 'POST':
+        selected_problems = request.POST.getlist('selected_problems')
+        for qid in selected_problems:
+            problem = Problem.objects.get(qid=qid)
+            # 문제의 정답을 사용자가 입력한 값으로 바꿈
+            problem.answer = request.POST.get('answer' + qid)
+            problem.save()
+        return redirect(reverse_lazy('exam:problem_list'))
+    return redirect(reverse_lazy('exam:exam'))
+
+def exam_judge(request):
+    problem_id = request.GET.get('problem_id', '')
+    problem = Hobt1.objects.get(qid=problem_id)
+    first_answer = request.GET.get('answer', '')
+    answer = problem.answer
+    similar_answer = problem.similar_answer
+    is_correct = first_answer.strip() == answer.strip()
+    context = {
+        'first_answer': first_answer,
+        'answer': answer,
+        'similar_answer': similar_answer,
+        'is_correct': is_correct,
+    }
+    return render(request, 'exam/exam_judge.html', context)
