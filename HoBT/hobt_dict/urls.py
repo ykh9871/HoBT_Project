09ab8d9ac8cd81b136b9bacce448cdbae873431a
yoon gyu heon <ykh9871@gmail.com>
2023-04-26 20:16:37 +0900

    HoBT Dict Add Feature

--- HoBT/hobt_dict/urls.py
+++ HoBT/hobt_dict/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('create/', views.HobtDictCreateView.as_view(), name='hobt_dict_create'), # 문제 등록
     path('<int:pk>/modify/', views.hobt_dict_modify, name='hobt_dict_modify'), # 문제 수정
     path('<int:pk>/delete/', views.hobt_dict_delete, name='hobt_dict_delete'), # 문제 삭제
-    path('like/', views.hobt_dict_like, name='hobt_dict_like'), # 문제 추천
+    path('like/<int:pk>/\\Z', views.hobt_dict_like, name='hobt_dict_like'), # 문제 추천
     path('<int:pk>/', views.hobt_dict_detail, name='hobt_dict_detail'), # 문제 상세보기
     path('search/', views.search, name='search') # 검색기능
 ]
