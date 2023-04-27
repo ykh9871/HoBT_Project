04815e93f9c7027f5136d1e83a787265893e89b4
Atopiano <121912367+Atopiano@users.noreply.github.com>
2023-04-27 20:35:11 +0900

    문제풀기 기능 정답 분기까지 구현 완료, exam_result 디자인 수정 필요.

--- HoBT/config/settings.py
+++ HoBT/config/settings.py
@@ -96,7 +96,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
-    {
+    {   
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
